1c2c6445a3137674db544851a508619dea74ae8d
eugenia <eugeniaalonsoc@gmail.com>
2022-06-20 20:16:06 -0300

    reinicie el proyecto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import "../styles/home.scss";
 import { Link } from "react-router-dom";
 
 const Home = () => {
@@ -7,11 +6,7 @@ const Home = () => {
         <div className='all-container'>
             <p className='pre-title'>¿Cuánto sabes sobre</p>
             <h1 className='title'>ballenas?</h1>
-            <button className='play-btn'>
-                <Link to="/Play" className='link'>
-                JUGAR
-                </Link>
-                </button>
+            
         </div>
     )
 }
